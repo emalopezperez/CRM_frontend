5bda26457023887cb1a7ce4a260a583b9d02c60b
emanuel lopez <emanuel-lopez13@hotmail.com>
2023-04-12 19:47:52 -0300

    Agregue sidebar al proyecto

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Sidebar from "@/components/sidebar/Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>CRM - Ecommerce</title>
+        <meta property="Emanuel Lopez" content="My page title" key="Agencia Software" />
+      </Head>
+      <Sidebar />
+      { children }
+    </>
+  )
+}
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout/Layout"
+
+const home = () => {
+  return (
+    <Layout classNameName="">index</Layout>
+  )
+}
+
+export default home
